cb88f768dc4c88907c3de24f88f2a1b612546dcc
Aliragenz <aliragenz@gmail.com>
2024-08-15 23:38:59 +0700

    Semua Jawaban

--- 3/3.js
+++ 3/3.js
@@ -2,7 +2,7 @@ function createPyramid(rows) {
     for (let i = 0; i < rows; i++) {
         var output = '';
         for (let b = 0; b < i; b++) output += ' '; // ngasih spasi di awal
-        for (let a = 0; a < (rows - i); a++) output += ' * ';
+        for (let a = 0; a < (rows - i); a++) output += ' + ';
         console.log(output);
     }
 }
